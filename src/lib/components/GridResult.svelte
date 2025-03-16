214308e7f1441130acec9e925cadab1b970bcff5
Florent <florent@nirlab.com>
2025-03-16 09:15:12 +0100

    Add popper content 101

--- src/lib/components/GridResult.svelte
+++ src/lib/components/GridResult.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import type { Cell, Grid } from "$lib/models";
+    import { onMount } from "svelte";
+    import { createPopperActions } from "svelte-popperjs";
+    import CellDetail from "./CellDetail.svelte";
 
     type Props = {
         grid: Grid;
@@ -34,6 +37,24 @@
             (week) => (week + 1) % aggregatedHeaders == 0
         );
     });
+
+    let popperDiv: HTMLDivElement | undefined = $state(undefined);
+    let popperCell: Cell | undefined = $state(undefined);
+
+    const [popperRef, popperContent] = createPopperActions();
+
+    function handleClickOutisde(e: MouseEvent) {
+        if (popperCell && popperDiv && e.target && !popperDiv?.contains(e.target as Node)) {
+            popperCell = undefined;
+        }
+    }
+
+    onMount(() => {
+        document.addEventListener("click", handleClickOutisde);
+        return () => {
+            document.removeEventListener("click", handleClickOutisde);
+        };
+    });
 </script>
 
 <table>
@@ -52,13 +73,26 @@
             <tr>
                 {#each year as cell}
                     <td class={classForCell(cell)}>
-                        <div></div>
+                        <button
+                            onclick={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                popperCell = cell;
+                                popperRef(e.currentTarget);
+                            }}
+                            aria-label="display week details"
+                        ></button>
                     </td>
                 {/each}
             </tr>
         {/each}
     </tbody>
 </table>
+{#if popperCell}
+    <div use:popperContent class="popper" bind:this={popperDiv}>
+        <CellDetail cell={popperCell} />
+    </div>
+{/if}
 
 <style>
     table {
@@ -81,26 +115,58 @@
         height: 5px;
     }
 
-    td > div {
+    td > button {
+        all: unset;
+        display: block;
+        background: none;
+        border: none;
+        padding: 0;
+        margin: 0;
+        font: inherit;
+        color: inherit;
+        cursor: pointer;
+
         background-color: lightgray;
         width: 100%;
         height: 100%;
         border-radius: 20%;
     }
 
-    td.birth > div {
+    td.birth > button {
         background-color: lightcoral;
     }
 
-    td.death > div {
+    td.death > button {
         background-color: lightcoral;
     }
 
-    td.life-event > div {
+    td.life-event > button {
         background-color: rgb(0, 255, 4);
     }
 
-    td.not-alive > div {
+    td.not-alive > button {
         opacity: 0.3;
+    }
+
+    td > button:hover {
+        background-color: grey;
+    }
+
+    .popper {
+        min-width: 5em;
+        min-height: 1em;
+
+        z-index: 1000;
+        overflow: auto;
+
+        /* Display popper div backend in white and dark mode along with visible text */
+        background-color: rgb(193, 192, 192);
+        padding: 0.5em 1em;
+        border-radius: 0.5em;
+
+        @media (prefers-color-scheme: dark) {
+            background-color: rgb(130, 129, 129);
+            color: white;
+        }
     }
 </style>
